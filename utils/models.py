789fea679b23a18b486d053957cbe8db852ed85a
Umbriona <sandra.viknander@gmail.com>
2021-03-01 18:37:03 +0100

    added config file

--- utils/models.py
+++ utils/models.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
-#import tensorflow_addons as tfa
 from tensorflow.keras import Model
-from tensorflow.keras.layers import Conv1D, Dense, Flatten, LeakyReLU, Embedding, Input, LeakyReLU, LayerNormalization, BatchNormalization, Softmax
-from utils.layers_new import SelfAttention, ResMod, Spectral_Norm, GumbelSoftmax 
+from tensorflow.keras.layers import Conv1D, Dense, Flatten, LeakyReLU, Embedding, Input, LeakyReLU, LayerNormalization, BatchNormalization, Softmax, Concatenate
+from utils.layers import SelfAttention, ResMod, Spectral_Norm, GumbelSoftmax
+
+
 
 class Generator_res(Model):
     def __init__(self, filters, size, dilation, vocab, use_gumbel, temperature = 0.5):
@@ -38,16 +39,110 @@ class Discriminator(Model):
         self.constraint = Spectral_Norm()
         self.act = LeakyReLU(0.2)
         self.conv1 = Conv1D(filters[0], size[0], strides=strides[0], padding='same', kernel_constraint = self.constraint, use_bias = False)
-        self.conv2 = Conv1D(filters[0], size[0], strides=strides[0], padding='same', kernel_constraint = self.constraint, use_bias = False)
-        self.conv3 = Conv1D(filters[0], size[0], strides=strides[0], padding='same', kernel_constraint = self.constraint, use_bias = False)
-        self.conv4 = Conv1D(filters[0], size[0], strides=strides[0], padding='same', kernel_constraint = self.constraint, use_bias = False)
+        self.conv2 = Conv1D(filters[1], size[0], strides=strides[0], padding='same', kernel_constraint = self.constraint, use_bias = False)
+        self.conv3 = Conv1D(filters[2], size[0], strides=strides[0], padding='same', kernel_constraint = self.constraint, use_bias = False)
+        self.conv4 = Conv1D(filters[3], size[0], strides=strides[0], padding='same', kernel_constraint = self.constraint, use_bias = False)
         self.atte = SelfAttention(vocab)
         self.conv = Conv1D(1, 4, strides=1, activation= activation, padding='same', kernel_constraint = self.constraint, use_bias = False)
+        self.cat = Concatenate(axis=-1)
         
-    def call(self, x):
+    def call(self, parent, child):
+        x = self.cat([parent, child])
         x, a_w = self.atte(x)
         x = self.act(self.conv1(x))
         x = self.act(self.conv2(x))
         x = self.act(self.conv3(x))
         x = self.act(self.conv4(x))
         return self.conv(x), a_w
+    
+class VirusGan(tf.keras.Model):
+
+    def __init__(self, config, callbacks=None):
+        super(CycleGan, self).__init__()
+        self.Generator, self.Discriminator = self.load_models(config['VirusGan'])
+         
+        self.add  = tf.keras.layers.Add()
+
+    def compile( self, loss_obj, optimizers):
+        
+        super(CycleGan, self).compile()
+        
+        self.gen_optimizer = optimizers['Generator']
+        self.disc_optimizer = optimizers['Discriminator']
+        
+        self.generator_loss_fn = loss_obj.generator_loss_fn
+        self.discriminator_loss_fn = loss_obj.discriminator_loss_fn
+
+    def load_models(self, config):
+        """Create all models that is used in cycle gan""" 
+
+        model_type = config["Generator"]["type"]
+        G_filters = config["Generator"]["filters"]
+        G_sizes   = config["Generator"]["kernels"]
+        G_dilation= config["Generator"]["dilations"]
+        G_gumbel = config["Generator"]["use_gumbel"]
+        G_temperature = config["Generator"]["temperature"]
+
+
+        D_filters = config["Discriminator"]["filters"]
+        D_sizes   = config["Discriminator"]["kernels"]
+        D_dilation= config["Discriminator"]["dilations"]
+        D_strides = config["Discriminator"]["strides"]
+
+        if config["Losses"]["loss"] == 'Non-Reducing':
+            D_activation = 'sigmoid'
+        else:
+            D_activation = 'linear'
+
+        vocab = config["Vocab_size"] 
+
+        Generator = Generator_res(G_filters, G_sizes, G_dilation, vocab, use_gumbel = G_gumbel, temperature = G_temperature)
+         
+        Discriminator = Discriminator(D_filters, D_sizes, D_strides, D_dilation, vocab, activation = D_activation)
+
+        return Generator, Discriminator
+    
+    @tf.function
+    def train_step(self, batch_data):
+
+        
+
+        with tf.GradientTape(persistent=True) as tape:
+            
+            # Batch data
+            parent, child, W = batch_data
+            
+            # Generator output
+            fake_child, _ = self.Generator(parent, training=True)
+
+            # Discriminator output
+            disc_real_child, _ = self.Discriminator(parent, child, training=True)
+            disc_fake_child, _ = self.Discriminator(parent, fake_child, training=True)
+            
+            # Loss for generator and discriminator
+            gen_loss = self.generator_loss_fn(disc_fake_child)
+            disc_loss = self.discriminator_loss_fn(disc_real_child, disc_fake_child)
+
+        # Get the gradients for the generator
+        gen_grads = tape.gradient(gen_loss, self.Generator.trainable_variables)
+       
+        # Get the gradients for the discriminators
+        disc_grads = tape.gradient(disc_loss, self.Discriminator.trainable_variables)
+
+
+        # Update the weights of the generator 
+        self.gen_optimizer.apply_gradients(zip(gen_grads, self.Generator.trainable_variables))  
+
+        # Update the weights of the discriminator
+        self.disc_optimizer.apply_gradients(zip(disc_grads, self.Discriminator.trainable_variables))
+
+
+        return {
+            "Generator_loss": gen_loss,
+            "Discriminator_loss": disc_loss,
+        }, fake_child
+    
+    #@tf.function
+    def validate_step(self, batch_data):
+        # TODO 
+        return 0
