98140b22401e7a4da37639a09ebfe752ad564f79
Umbriona <sandra.viknander@gmail.com>
2021-03-01 11:51:08 +0100

    Adding layers module

---
+++ utils/layers.py
@@ -0,0 +1,104 @@
+import tensorflow as tf
+from tensorflow.keras.constraints import Constraint
+from tensorflow.keras.layers import Layer, Conv1D, LayerNormalization, Add, Concatenate, LeakyReLU, Softmax
+import tensorflow_probability as tfp
+
+POWER_ITERATIONS = 5
+
+class Spectral_Norm(Constraint):
+    '''
+    Uses power iteration method to calculate a fast approximation 
+    of the spectral norm (Golub & Van der Vorst)
+    The weights are then scaled by the inverse of the spectral norm
+    '''
+    def __init__(self, power_iters=POWER_ITERATIONS):
+        self.n_iters = power_iters
+
+    def __call__(self, w):
+        flattened_w = tf.reshape(w, [w.shape[0], -1])
+        u = tf.random.normal([flattened_w.shape[0]])
+        v = tf.random.normal([flattened_w.shape[1]])
+        for i in range(self.n_iters):
+            v = tf.linalg.matvec(tf.transpose(flattened_w), u)
+            v = tf.keras.backend.l2_normalize(v)
+            u = tf.linalg.matvec(flattened_w, v)
+            u = tf.keras.backend.l2_normalize(u)
+            sigma = tf.tensordot(u, tf.linalg.matvec(flattened_w, v), axes=1)
+        return w / sigma
+
+    def get_config(self):
+        return {'n_iters': self.n_iters}
+    
+class GumbelSoftmax(Layer):
+    """
+    Activation layer produces an approximate one hot encoded sample of the softmax for a which gradients are not zero as in the
+    argmax function
+    
+    __init__()
+    
+    temperature: variable set to how agressiv the approximation of onehot vector should be
+    
+    call()
+    
+    logits: the linear output from your model 
+    """
+    def __init__(self,temperature = 0.5, *args, **kwargs):
+        super(GumbelSoftmax,self).__init__()
+        
+        # Temperature
+        self.tau = temperature
+    
+    def call(self, logits):
+        U = tf.random.uniform(tf.shape(logits), minval=0, maxval=1, dtype=tf.dtypes.float32)
+        g = -tf.math.log(-tf.math.log(U+1e-20)+1e-20)
+        nom = tf.keras.activations.softmax((g + logits)/self.tau, axis=-1)
+        return nom
+    
+class ResMod(Layer):
+    """
+    Residual module 
+    """
+    def __init__(self, filters, size, strides=1, dilation=1, constrains = None):
+        super(ResMod, self).__init__()
+        
+        self.conv1 = Conv1D(filters, size,
+                            dilation_rate = dilation,
+                            padding = 'same',
+                            use_bias = False,
+                            kernel_constraint = constrains )
+        self.conv2 = Conv1D(filters, size, dilation_rate = dilation, padding = 'same',
+                            use_bias = False,
+                            kernel_constraint = constrains )
+        self.conv3 = Conv1D(filters, size, dilation_rate = dilation, padding = 'same',
+                            use_bias = False,
+                            kernel_constraint = constrains )
+        self.strides = False
+        
+        if strides > 1:
+            self.strides = True
+            self.conv4 = Conv1D(filters, size, dilation_rate = 1, strides = strides, padding = 'same',
+                            use_bias = False,
+                            kernel_constraint = constrains )
+            
+            
+        self.conv  = Conv1D(filters, 1, padding = 'same',
+                            use_bias = False,
+                            kernel_constraint = constrains )   
+        self.add = Add()
+        
+
+        #self.norm = InstanceNormalization(
+        #                           beta_initializer="random_uniform",
+        #                           gamma_initializer="random_uniform")
+        self.act = LeakyReLU(0.2)
+        
+    def call(self, x):
+        x_in = self.conv(x)
+        x = self.act(self.conv1(x))
+        x = self.act(self.conv2(x))
+        x = self.act(self.conv3(x))
+        x = self.add([x, x_in])
+        if self.strides:
+            x = self.act(self.conv4(x)) 
+        x = self.act(x)
+        return x
